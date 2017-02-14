1e09676b3940c18fa31fb2fc618dad888431c539
cenjunyi <cen.jy@cfischina.com>
2017-02-14 16:14:52 +0800

    更改图标, 并且服务器改为新浪云的服务器

--- pages/near/near.js
+++ pages/near/near.js
@@ -1,22 +1,22 @@
 // pages/near/near.js
 //获取应用实例
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
-function toRad(d) {  return d * Math.PI / 180; }
+function toRad(d) { return d * Math.PI / 180; }
 
-function getDistance(lat1, lng1, lat2, lng2) { 
+function getDistance(lat1, lng1, lat2, lng2) {
   //#lat为纬度, lng为经度, 一定不要弄错
-    var dis = 0;
-    var radLat1 = toRad(lat1);
-    var radLat2 = toRad(lat2);
-    var deltaLat = radLat1 - radLat2;
-    var deltaLng = toRad(lng1) - toRad(lng2);
-    var dis = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(deltaLat / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(deltaLng / 2), 2)));
-    return Math.round(dis * 6378137);
+  var dis = 0;
+  var radLat1 = toRad(lat1);
+  var radLat2 = toRad(lat2);
+  var deltaLat = radLat1 - radLat2;
+  var deltaLng = toRad(lng1) - toRad(lng2);
+  var dis = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(deltaLat / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(deltaLng / 2), 2)));
+  return Math.round(dis * 6378137);
 }
 var app = getApp()
 Page({
   data: {
-    tabs: ["网点", "自助银行", "可预约",""],
+    tabs: ["网点", "自助银行", "可预约", ""],
     activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0,
@@ -31,27 +31,27 @@ Page({
       latitude: '23.1065995692',
       longitude: '113.3244326464',
     },
-    map:{
-      width:100,
-      height:100
+    map: {
+      width: 100,
+      height: 100
     },
     hidden: true,
-    bank:'',
-    list:[],
+    bank: '',
+    list: [],
     markers: [],
-    netinfos:[],
-    sel_marker:{},
+    netinfos: [],
+    sel_marker: {},
     controls: [{
-        id: 2,
-        iconPath: '../../image/location2.png',
-        position: {
-          left: 20,
-          top: 450,
-          width: 30,
-          height: 30
-        },
-        clickable: true
-      }]
+      id: 2,
+      iconPath: '../../image/location2.png',
+      position: {
+        left: 20,
+        top: 450,
+        width: 30,
+        height: 30
+      },
+      clickable: true
+    }]
   },
   onShareAppMessage: function () {
     return {
@@ -64,7 +64,7 @@ Page({
     var that = this
     console.log(e)
     console.log(e.type)
-    if(e.type == 'end'){
+    if (e.type == 'end') {
       that.getData();
     }
   },
@@ -72,22 +72,22 @@ Page({
     console.log(e.markerId)
     this.lightMarker(e.markerId)
     this.showDetail(e.markerId)
-    
+
   },
-  maptap(e){
+  maptap(e) {
     console.log('maptap')
   },
   controltap(e) {
     var that = this
     console.log(e.controlId)
     // this.showList()
-    if(e.controlId == 1){
+    if (e.controlId == 1) {
       that.moveToLocation()
     }
-    if(e.controlId == 2){
+    if (e.controlId == 2) {
       that.resetLocation()
     }
-    if(e.controlId == 3){
+    if (e.controlId == 3) {
       that.showList()
     }
 
@@ -97,8 +97,8 @@ Page({
     // })
   },
   radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：',    e.detail.value);
-    this.data['bank']=e.detail.value
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+    this.data['bank'] = e.detail.value
   },
   confirm: function () {
     var key = 'hidden'
@@ -107,27 +107,27 @@ Page({
       this.data[key] === false
     this.setData(changedData)
   },
-  showDetail:function(mid){
+  showDetail: function (mid) {
     var that = this
     var arr_list = new Array()
     that.getMarker(mid)
-    var distance = getDistance(that.data['cur_location'].latitude,that.data['cur_location'].longitude,that.data['sel_marker'].latitude,that.data['sel_marker'].longitude)
+    var distance = getDistance(that.data['cur_location'].latitude, that.data['cur_location'].longitude, that.data['sel_marker'].latitude, that.data['sel_marker'].longitude)
     console.log(that.data['sel_marker'])
     arr_list.push(that.data['sel_marker'].name)
     arr_list.push(that.data['sel_marker'].desc)
-    arr_list.push("距离："+distance+"米，导航")
+    arr_list.push("距离：" + distance + "米，导航")
     arr_list.push("取号")
     wx.showActionSheet({
-      itemList:arr_list,
-      success: function(res) {
+      itemList: arr_list,
+      success: function (res) {
         console.log(res.tapIndex)
-        if(res.tapIndex == 0 ) {
-          
-        }
-        if(res.tapIndex == 1 ) {
+        if (res.tapIndex == 0) {
 
         }
-        if(res.tapIndex == 2 ) {
+        if (res.tapIndex == 1) {
+
+        }
+        if (res.tapIndex == 2) {
           wx.openLocation({
             longitude: Number(that.data['sel_marker'].longitude),
             latitude: Number(that.data['sel_marker'].latitude),
@@ -135,34 +135,34 @@ Page({
             address: that.data['sel_marker'].desc
           })
         }
-        if(res.tapIndex == 3 ) {
+        if (res.tapIndex == 3) {
           wx.navigateTo({
             url: '../queue/queue',
           })
         }
       },
-      fail: function(res) {
+      fail: function (res) {
         console.log(res.errMsg)
       }
     })
 
   },
-  showList:function(){
+  showList: function () {
     console.log(this.data['list'])
 
     var arrmids = new Array()
-    this.data['markers'].forEach(function(e){ 
+    this.data['markers'].forEach(function (e) {
       arrmids.push(e.id)
     })
- 
+
     wx.navigateTo({
-      url: '../list/list?id='+arrmids,
+      url: '../list/list?id=' + arrmids,
     })
   },
   getCenterLocation: function () {
     var that = this
     this.mapCtx.getCenterLocation({
-      success: function(res){
+      success: function (res) {
         console.log(res.longitude)
         console.log(res.latitude)
         that.setData({
@@ -175,72 +175,72 @@ Page({
       }
     })
   },
-  lightMarker:function(mid){
+  lightMarker: function (mid) {
     var that = this
     var arr_marker = new Array()
-    that.data['markers'].forEach(function(e){
+    that.data['markers'].forEach(function (e) {
       // console.log(e.id)
-      if(e.id == mid ){
+      if (e.id == mid) {
         arr_marker.push({
-          id:e.id,
-          latitude:e.latitude,
-          longitude:e.longitude,
-          width:20,
-          height:30,
-          iconPath:'../../image/red.png',
-          name:e.name,
-          desc:e.desc
-        });        
+          id: e.id,
+          latitude: e.latitude,
+          longitude: e.longitude,
+          width: 20,
+          height: 30,
+          iconPath: '../../image/red.png',
+          name: e.name,
+          desc: e.desc
+        });
       } else {
         arr_marker.push({
-          id:e.id,
-          latitude:e.latitude,
-          longitude:e.longitude,
-          width:20,
-          height:30,
-          iconPath:'../../image/green.png',
-          name:e.name,
-          desc:e.desc
+          id: e.id,
+          latitude: e.latitude,
+          longitude: e.longitude,
+          width: 20,
+          height: 30,
+          iconPath: '../../image/green.png',
+          name: e.name,
+          desc: e.desc
         });
       }
     })
-    console.log('arr_marker:'+arr_marker)
+    console.log('arr_marker:' + arr_marker)
     that.setData({
       markers: arr_marker
     })
   },
-  getMarker:function(mid){
+  getMarker: function (mid) {
     var that = this
-    that.data['markers'].forEach(function(obj){
-      if(obj.id == mid ){
+    that.data['markers'].forEach(function (obj) {
+      if (obj.id == mid) {
         console.log(obj)
         that.setData({
-          sel_marker:obj
+          sel_marker: obj
         })
-      } 
+      }
     })
   },
-  getNetname:function(mid){
+  getNetname: function (mid) {
     var that = this
     var res = ""
-    that.data['markers'].forEach(function(e){
+    that.data['markers'].forEach(function (e) {
       console.log(e.id)
       console.log(mid)
-      if(e.id == mid ){
+      if (e.id == mid) {
         res = e.name
-      } 
+      }
     })
     return res
   },
-  getNetdesc:function(mid){
+  getNetdesc: function (mid) {
     var that = this
     var res = ""
-    that.data['markers'].forEach(function(e){
+    that.data['markers'].forEach(function (e) {
       console.log(e.id)
       console.log(mid)
-      if(e.id == mid ){
+      if (e.id == mid) {
         res = e.desc
-      } 
+      }
     })
     return res
   },
@@ -259,16 +259,16 @@ Page({
         height: 20,
         name: 'T.I.T 创意园',
         desc: '我现在的位置'
-      },{
-        id:'1',
+      }, {
+        id: '1',
         latitude: 23.1065995692,
         longitude: 113.3244326464,
         width: 20,
         height: 20,
         name: '广州塔',
         desc: '广州塔'
-      },{
-        id:'2',
+      }, {
+        id: '2',
         latitude: 23.1027375692,
         longitude: 113.3274466464,
         width: 20,
@@ -276,13 +276,13 @@ Page({
         name: '珠江帝景',
         desc: '珠江帝景'
       }],
-      list:['T.I.T 创意园','广州塔','珠江帝景']
+      list: ['T.I.T 创意园', '广州塔', '珠江帝景']
     })
-    this.data['markers'].forEach(function(e){ 
+    this.data['markers'].forEach(function (e) {
 
       wx.setStorage({
-        key:e.id,
-        data:e
+        key: e.id,
+        data: e
       })
 
     })
@@ -304,77 +304,44 @@ Page({
     })
     that.mapCtx.moveToLocation()
   },
-  testReq:function(){
-    var that = this
-    wx.request({
-      url: 'https://skipper.applinzi.com/api',
-      data: {},
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
-      success: function (res) {
-        console.log(res.data)
-        that.data['content'] = res.data
-        that.setData({
-          content: res.data
-        })
-      },
-      fail: function () {
-        console.log("fail")
-      },
-      complete: function () {
-        // complete
-        console.log("complete")
-      }
-    })
-  },
-  getData:function(){
+
+  getData: function () {
     var that = this;
     that.getCenterLocation()
     wx.request({
-      url: 'https://eapply.abchina.com/entapply/api/values/Get', //仅为示例，并非真实的接口地址
+      // url: 'https://eapply.abchina.com/entapply/api/values/Get', //仅为示例，并非真实的接口地址
+      url: 'https://skipper.applinzi.com/api/net2', //仅为示例，并非真实的接口地址
       method: 'GET',
       data: {
-        logitude:this.data['location'].latitude,
-        latitude:this.data['location'].longitude,
-        distance:'5000',
-        businessType:'2'
+        logitude:this.data['location'].longitude ,
+        latitude:this.data['location'].latitude ,
+        distance: '5000',
+        businessType: '2'
       },
       header: {
-          'content-type': 'application/json'
+        'content-type': 'application/json'
       },
-      success: function(res) {
-        console.log(res.data.BranchSearchRests)
-        // var netinfos = wx.getStorage('netinfos') || []
-        // logs.unshift(netinfos)
-        // wx.setStorageSync('logs', logs)
-
+      success: function (res) {
+        console.log("res.data" + res.data)
         var arr_marker = new Array()
         var arr_list = new Array()
-        res.data.BranchSearchRests.forEach(function(e){ 
-          // try {
-          //     wx.setStorage(e.BranchId, e)
-          //     } catch (e) {
-          // } 
-          // wx.setStorage({
-          //   key:e.BranchId,
-          //   data:e
-          // })
+        res.data.forEach(function (e) {
           arr_marker.push({
-            id:e.BranchId,
-            latitude:e.Latitude,
-            longitude:e.Longitude,
-            width:20,
-            height:30,
-            iconPath:'../../image/green.png',
-            name:e.Name,
-            desc:e.FullAddress
+            // id:e.BranchId,
+            latitude: e.Latitude,
+            longitude: e.Longitude,
+            width: 20,
+            height: 30,
+            iconPath: '../../image/green.png',
+            name: e.Name,
+            desc: e.机构地址
           });
           arr_list.push(e.Name)
         })
         that.setData({
           markers: arr_marker,
-          list:arr_list,
-          netinfos:res.data.BranchSearchRests
+          list: arr_list,
+          netinfos: res.data.BranchSearchRests
         })
       }
     })
@@ -395,9 +362,9 @@ Page({
   // },
   getLocation: function () {
     wx.chooseLocation({
-        success: function (obj) {
-          console.log(res.name)
-        }
+      success: function (obj) {
+        console.log(res.name)
+      }
     })
   },
   //事件处理函数
@@ -409,31 +376,30 @@ Page({
   //排队选择
   paidui: function () {
     wx.navigateTo({
-      url: '../paidui/paidui?id='+this.data['bank']
+      url: '../paidui/paidui?id=' + this.data['bank']
     })
   },
   onLoad: function () {
     // 页面初始化 options为页面跳转所带来的参数
     var that = this;
     wx.getSystemInfo({
-        success: function(res) {
-            that.setData({
-                sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-                sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex,
-                map:{
-                  width:res.windowWidth,
-                  height:res.windowHeight
-                },
-                cur_location:{
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex,
+          map: {
+            width: res.windowWidth,
+            height: res.windowHeight
+          },
+          cur_location: {
 
-                }
-            });
-        }
+          }
+        });
+      }
     });
     console.log('onLoad')
     console.log(that.data['map'].width)
-    that.testReq()
-  },    
+  },
   tabClick: function (e) {
     console.log(e);
     var that = this
@@ -441,7 +407,7 @@ Page({
     if (e.currentTarget.id == 1) {
       that.moveToLocation()
     }
-    if (e.currentTarget.id == 2||e.currentTarget.id == 3) {
+    if (e.currentTarget.id == 2 || e.currentTarget.id == 3) {
       that.showList()
     }
     // this.setData({
@@ -454,15 +420,15 @@ Page({
     console.log('onready')
     console.log(this.data['netinfos'])
     // 使用 wx.createMapContext 获取 map 上下文 
-    this.data['netinfos'].forEach(function(e){ 
+    this.data['netinfos'].forEach(function (e) {
       wx.setStorage({
-        key:e.BranchId,
-        data:e
+        key: e.BranchId,
+        data: e
       })
     })
     var that = this
     that.mapCtx = wx.createMapContext('myMap')
-    
+
     wx.getLocation({
       success: function (res) {
         console.log(res)
